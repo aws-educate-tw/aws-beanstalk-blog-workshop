ae27a6858680c55d6482084cb1379c9e35709378
sh1un <a0923183408@gmail.com>
2023-11-30 23:58:43 +0800

    feat: initialize Spring Boot project

---
+++ src/main/java/tw/awseducate/awsbeanstalkblogworkshop/AwsBeanstalkBlogWorkshopApplication.java
@@ -0,0 +1,13 @@
+package tw.awseducate.awsbeanstalkblogworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstalkBlogWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstalkBlogWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/awseducate/awsbeanstalkblogworkshop/AwsBeanstalkBlogWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.awseducate.awsbeanstalkblogworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBeanstalkBlogWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
